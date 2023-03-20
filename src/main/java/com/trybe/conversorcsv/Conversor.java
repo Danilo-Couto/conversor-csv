253c2e9874ff511e0cb40c74f97b8aa36158db6f
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-03-20 14:38:20 -0300

    Initial commit

---
+++ src/main/java/com/trybe/conversorcsv/Conversor.java
@@ -0,0 +1,35 @@
+package com.trybe.conversorcsv;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Conversor {
+
+  /**
+   * Função utilizada apenas para validação da solução do desafio.
+   *
+   * @param args Não utilizado.
+   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou
+   *                     gravar os arquivos de saída.
+   */
+  public static void main(String[] args) throws IOException {
+    File pastaDeEntradas = new File("./entradas/");
+    File pastaDeSaidas = new File("./saidas/");
+    new Conversor().converterPasta(pastaDeEntradas, pastaDeSaidas);
+  }
+
+  /**
+   * Converte todos os arquivos CSV da pasta de entradas. Os resultados são gerados
+   * na pasta de saídas, deixando os arquivos originais inalterados.
+   *
+   * @param pastaDeEntradas Pasta contendo os arquivos CSV gerados pela página web.
+   * @param pastaDeSaidas Pasta em que serão colocados os arquivos gerados no formato
+   *                      requerido pelo subsistema.
+   *
+   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou
+   *                     gravar os arquivos de saída.
+   */
+  public void converterPasta(File pastaDeEntradas, File pastaDeSaidas) throws IOException {
+    // TODO: Implementar.
+  }
+}
